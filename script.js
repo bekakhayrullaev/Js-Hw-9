f40122ea34caf17fbb03a56580fb4cafdfa8befb
bekakhayrullaev <bekahajrullaev27@gmail.com>
2022-03-24 20:48:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+for(i = 0; i < 5; i++){
+    let ul = ''
+    for(k = 0;  k < 5 - i; k++){
+        ul += '*'
+
+    }
+    console.log(ul);
+}
+for(i = 5; i > 0; i--){
+    let ul = ''
+    for(k = 0;  k <= 5 - i; k++){
+        ul += '*'
+
+    }
+    console.log(ul);
+}
+
+
+
